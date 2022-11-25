736d59a8d1e873423116cdda292b1b03c7e30bcc
rambutan <pride.kraievskyi@gmail.com>
2022-11-25 15:01:35 +0200

    try

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -9,6 +9,7 @@ export class ConfigService implements IConfigService {
 	private readonly config: DotenvParseOutput;
 	constructor(@inject(TYPES.ILogger) private logger: ILogger) {
 		const result: DotenvConfigOutput = config();
+		console.log(process.env);
 		if(process.env) {
 			this.logger.log(process.env)
 		}
