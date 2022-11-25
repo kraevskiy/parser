51b9aa71e7fc8c53b38f5a214dff8877172528fc
rambutan <pride.kraievskyi@gmail.com>
2022-11-25 15:03:30 +0200

    try

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -9,16 +9,13 @@ export class ConfigService implements IConfigService {
 	private readonly config: DotenvParseOutput;
 	constructor(@inject(TYPES.ILogger) private logger: ILogger) {
 		const result: DotenvConfigOutput = config();
-		console.log(process.env);
-		if(process.env) {
-			this.logger.log(process.env)
-		}
 		if (result.error) {
 			this.logger.error(`[ConfigService] File .env parse error or didn't find this file`);
 		} else {
 			this.logger.log('[ConfigService] Configuration .end downloaded');
 			this.config = result.parsed as DotenvParseOutput;
 		}
+
 	}
 
 	get(key: string): string {
