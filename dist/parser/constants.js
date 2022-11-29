77e6d11758b0d038c7e4ecdc0c3e032e9abf9e06
rambutan <pride.kraievskyi@gmail.com>
2022-11-29 12:12:39 +0200

    try

--- dist/parser/constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    url: (vin) => `https://w8shippingua.com/tracking/?lot=&vin=${vin}&searchAuto=Search+%C2%BB`,
-    tablesSelector: 'td.details-cell',
-    photosSelector: '.photos__gallery .gallery__link'
-};
-//# sourceMappingURL=constants.js.map
